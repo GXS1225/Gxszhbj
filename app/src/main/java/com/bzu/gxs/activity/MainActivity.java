cb9a9355135e09a84f0efb6696cc56bef7a8473f
GXS <1396795023@qq.com>
2016-05-31 22:14:58 +0800

    init

--- app/src/main/java/com/bzu/gxs/activity/MainActivity.java
+++ app/src/main/java/com/bzu/gxs/activity/MainActivity.java
@@ -2,7 +2,6 @@ package com.bzu.gxs.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import com.bzu.gxs.R;
 
 public class MainActivity extends AppCompatActivity {
